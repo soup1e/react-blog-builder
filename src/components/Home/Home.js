86031a5268c57dbe309971d48e5bd6cfd21625af
Sam Adams <sam.adams7785@gmail.com>
2023-01-11 12:36:27 -0800

    title state variable

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
@@ -8,11 +9,17 @@ import './Home.css';
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
 
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubTitle] = useState('');
+  const [font, setFont] = useState('');
+  const [align, setAlign] = useState('');
+  const [text, setText] = useState('');
+
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview title={title} />
+      <Editor setTitle={setTitle} />
     </main>
   );
 }
--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview() {
+export default function Preview({ title }) {
   // implement a Preview screen here
   //  Note - the HTML should have the following structure
   //  the main div should have a class of preview and the font-name prop
   //  and should have style interpolated using the alignment prop
 
   // <div class="preview indie-flower" style="text-align: center;">
-  //   <h1> Blog Title</h1>
-  //   <h3> Blog Subtitle</h3>
-  //   <p>This is some blog text. </p>
-  // </div>
 
-  return <></>;
+  // <h3> Blog Subtitle</h3>
+  // <p>This is some blog text. </p>
+  // </div>;
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
 }
